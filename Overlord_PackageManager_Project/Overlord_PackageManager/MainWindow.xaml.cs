d15a4f600d02a76e4f64aaf4d944d89095dd2341
Kull_Warrior <92987811+Kull-Warrior@users.noreply.github.com>
2026-02-18 22:24:07 +0100

    Implemented GUI I/O

--- Overlord_PackageManager_Project/Overlord_PackageManager/MainWindow.xaml.cs
+++ Overlord_PackageManager_Project/Overlord_PackageManager/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Overlord_PackageManager.resources;
+﻿using Microsoft.Win32;
+using Overlord_PackageManager.resources;
 using Overlord_PackageManager.resources.OMP;
 using Overlord_PackageManager.resources.RPK;
+using System.IO;
 using System.Windows;
 
 namespace Overlord_PackageManager
@@ -10,22 +12,72 @@ namespace Overlord_PackageManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        ResourcePackFile resourceFile;
+        OMPFile mapFile;
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            OMPFile mapFile = new OMPFile();
-            //mapFile.Parse("D:\\Downloads\\26092025\\OL\\OMP\\Tower.omp");
+        private void Browse_File_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "RPK Files (*.rpk)|*.rpk|" +
+                "PRP Files (*.prp)|*.prp|" +
+                "PSP Files (*.psp)|*.psp|" +
+                "PVP Files (*.pvp)|*.pvp|" +
+                "OMP Files (*.omp)|*.omp|" +
+                "All files (*.*)| *.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath.Text = openFileDialog.FileName;
+            }
+        }
 
-            string baseDir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Overlord\\Resources\\";
-            string fileName = "System Content1";
-            string fileExtension = ".prp";
-            ResourcePackFile file = new ResourcePackFile();
-            file.Read(baseDir + fileName + fileExtension);
-            file.WriteAllAssetsToFile(baseDir + fileName);
-            treeView.Items.Clear();
-            treeView.Items.Add(RefTableTreeBuilder.Build(file.Body.Data, "Root"));
-            Console.WriteLine();
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(filePath.Text) || !File.Exists(filePath.Text))
+            {
+                MessageBox.Show("Please select a valid resource file.");
+                return;
+            }
+
+            if (filePath.Text.EndsWith(".omp"))
+            {
+                mapFile = new OMPFile();
+                mapFile.Parse(filePath.Text);
+                treeView.Items.Clear();
+                treeView.Items.Add(RefTableTreeBuilder.Build(mapFile.Body.Data, "Root"));
+            }
+            else
+            {
+                resourceFile = new ResourcePackFile();
+                resourceFile.Read(filePath.Text);
+                treeView.Items.Clear();
+                treeView.Items.Add(RefTableTreeBuilder.Build(resourceFile.Body.Data, "Root"));
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(filePath.Text) || !File.Exists(filePath.Text))
+            {
+                MessageBox.Show("Please select a valid resource file.");
+                return;
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(filePath.Text);
+            string parentDir = dirInfo.Parent.FullName + "\\";
+            string dirName = Path.GetFileNameWithoutExtension(filePath.Text);
+
+            if (filePath.Text.EndsWith(".omp"))
+            {
+                //mapFile.WriteAllAssetsToFile(parentDir + dirName);
+            }
+            else
+            {
+                resourceFile.WriteAllAssetsToFile(parentDir + dirName);
+            }
         }
     }
 }
