3097e0545bfcdd5439773b312a415e82b8024345
Kull_Warrior <92987811+Kull-Warrior@users.noreply.github.com>
2026-02-27 22:44:20 +0100

    Further UI refactoring

--- Overlord_PackageManager_Project/Overlord_PackageManager/resources/EntryEditor/Image/DDS/DDSMipChainEditor.xaml.cs
+++ Overlord_PackageManager_Project/Overlord_PackageManager/resources/EntryEditor/Image/DDS/DDSMipChainEditor.xaml.cs
@@ -1,73 +1,96 @@
-﻿using System.Windows.Controls;
-using System.Windows;
-using Overlord_PackageManager.resources.EntryTypes.BaseTypes;
+﻿using Overlord_PackageManager.resources.EntryTypes.BaseTypes;
 using Overlord_PackageManager.resources.EntryTypes.Image.DDS;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Overlord_PackageManager.resources.EntryEditor
 {
     public partial class DDSMipChainEditor : UserControl
     {
+        private readonly List<MipLevelData> _mips = new();
+        private int _currentIndex = 0;
+
         public DDSMipChainEditor(ListOfDDSTextures list)
         {
             InitializeComponent();
 
-            var ddsEntries = list.Table.Entries
-                .OfType<DDSTextures>()
-                .ToList();
+            List<DDSTextures> ddsEntries = list.Table.Entries.OfType<DDSTextures>().ToList();
 
             int level = 0;
 
             foreach (var dds in ddsEntries)
             {
-                var intEntries = dds.Table.Entries
-                    .OfType<Int32Entry>()
-                    .ToList();
+                List<Int32Entry> intEntries = dds.Table.Entries.OfType<Int32Entry>().ToList();
 
                 if (intEntries.Count < 3)
                     continue;
 
-                var lastThree = intEntries.TakeLast(3).ToList();
+                List<Int32Entry> lastThree = intEntries.TakeLast(3).ToList();
 
                 uint width = lastThree[0].varInt;
                 uint height = lastThree[1].varInt;
                 DDSFormat format = (DDSFormat)lastThree[2].varInt;
 
-                var blob = dds.Table.Entries
-                    .OfType<BlobEntry>()
-                    .FirstOrDefault();
+                BlobEntry? blob = dds.Table.Entries.OfType<BlobEntry>().FirstOrDefault();
 
                 if (blob == null)
                     continue;
 
-                var viewer = new DDSImageViewer(width, height, format, blob.varBytes);
+                _mips.Add(new MipLevelData(width, height, format, blob.varBytes));
 
-                var panel = CreateMipPanel(level, width, height, format, viewer);
-
-                MipContainer.Children.Add(panel);
+                MipSelector.Items.Add(
+                    $"Level {level}  ({width}x{height})");
 
                 level++;
             }
+
+            MipSelector.SelectionChanged += MipSelector_SelectionChanged;
+            PreviewImage.MouseLeftButtonDown += PreviewImage_MouseLeftButtonDown;
+
+            if (_mips.Count > 0)
+                MipSelector.SelectedIndex = 0;
         }
 
-        private UIElement CreateMipPanel(int level, uint width, uint height, DDSFormat format, DDSImageViewer viewer)
+        private void MipSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var container = new StackPanel
+            if (MipSelector.SelectedIndex < 0)
+                return;
+
+            _currentIndex = MipSelector.SelectedIndex;
+            RenderCurrent();
+        }
+
+        private void RenderCurrent()
+        {
+            if (_mips.Count == 0)
+                return;
+
+            MipLevelData mip = _mips[_currentIndex];
+
+            byte[] rgba = DDSDecoder.Decode(mip.Width, mip.Height, mip.Format, mip.Data);
+
+            WriteableBitmap bmp = new WriteableBitmap((int)mip.Width, (int)mip.Height, 96, 96, PixelFormats.Bgra32, null);
+
+            bmp.WritePixels(new Int32Rect(0, 0, (int)mip.Width, (int)mip.Height), rgba, (int)mip.Width * 4, 0);
+
+            PreviewImage.Source = bmp;
+
+            WidthText.Text = $"Width: {mip.Width}";
+            HeightText.Text = $"Height: {mip.Height}";
+            FormatText.Text = $"{mip.Format}";
+        }
+
+        private void PreviewImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && _mips.Count > 0)
             {
-                Orientation = Orientation.Vertical,
-                Margin = new Thickness(10)
-            };
-
-            var label = new TextBlock
-            {
-                Text = $"Level {level}\n{width}x{height}\n{format}",
-                Margin = new Thickness(0, 0, 0, 5),
-                FontFamily = new System.Windows.Media.FontFamily("Consolas")
-            };
-
-            container.Children.Add(label);
-            container.Children.Add(viewer);
-
-            return container;
+                MipLevelData mip = _mips[_currentIndex];
+                DDSZoomWindow window = new DDSZoomWindow(mip);
+                window.Show();
+            }
         }
     }
 }
---
+++ Overlord_PackageManager_Project/Overlord_PackageManager/resources/EntryEditor/Image/DDS/MipLevelData.cs
@@ -0,0 +1,20 @@
+﻿using Overlord_PackageManager.resources.EntryTypes.Image.DDS;
+
+namespace Overlord_PackageManager.resources.EntryEditor
+{
+    public class MipLevelData
+    {
+        public uint Width { get; }
+        public uint Height { get; }
+        public DDSFormat Format { get; }
+        public byte[] Data { get; }
+
+        public MipLevelData(uint width, uint height, DDSFormat format, byte[] data)
+        {
+            Width = width;
+            Height = height;
+            Format = format;
+            Data = data;
+        }
+    }
+}
