da177ce21b1f8dd41d33eaf119c6caca9444043c
Kull_Warrior <92987811+Kull-Warrior@users.noreply.github.com>
2026-07-16 15:42:25 +0200

    Implemented a simple Object class

---
+++ Overlord_PackageManager_Project/Overlord_PackageManager/resources/Data/Factories/ObjectFactory.cs
@@ -0,0 +1,29 @@
+﻿using Overlord_PackageManager.resources.Data.EntryTypes.Leaf;
+using Overlord_PackageManager.resources.Data.Generic;
+using System.IO;
+
+namespace Overlord_PackageManager.resources.Data.Factories
+{
+    public abstract class ObjectFactory
+    {
+        public static Entry CreateObjectAsset(BinaryReader reader, uint id, uint relOffset)
+        {
+            return id switch
+            {
+                01 => new BlobEntry(id, relOffset),
+                19 => new Int32Entry(id, relOffset),
+                20 => new StringEntry(id, relOffset),
+                21 => new StringEntry(id, relOffset),
+                30 => new TableEntry(id, relOffset),
+                32 => new FloatArrayEntry(id, relOffset),
+                33 => new TableEntry(id, relOffset),
+                34 => new FloatArrayEntry(id, relOffset),
+                35 => new TableEntry(id, relOffset),
+                36 => new BlobEntry(id, relOffset),
+
+                // Add more IDs here
+                _ => new BlobEntry(id, relOffset),   // Unknown entry
+            };
+        }
+    }
+}
